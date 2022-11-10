7d0556667efb8abd67a0b11738c7fb7ff89978b1
Noah <nhilaire@gaming.tech>
2022-11-10 19:32:48 +0100

    exos

--- tableau.py
+++ tableau.py
@@ -1,10 +1,16 @@
 
 def tableau(x,y,t):
-    tab = [[0] * t] * t
-    tab[0][1] = 1
+    tab = [[0 for j in range(t)] for i in range(t)]
+    tab[x-1][y] = 1
+    tab[x][y] = 1
+    tab[t-(t-x-1)][y] = 1
+    tab[x][y-1] = 1
+    tab[x][t-(t-y-1)] = 1
     for i in range(len(tab)):
         print(tab[i])
-    return tab
 
 
-print(tableau(1,1,3))
+print(tableau(0,1,3))
+
+t=[5,10,7,9,7,8]
+print(t[4-2])
