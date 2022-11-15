6eb62ff9438f478b38ebfef940736963adf50182
Noah <nhilaire@gaming.tech>
2022-11-15 13:17:56 +0100

    projet

--- morpion.py
+++ morpion.py
@@ -2,94 +2,155 @@
 
 
 #on definit une fonction morpion
-def morpion():
+def morpion(nombreJoueur):
     #on initialise un tableau avec 3 listes qui contiennent 3 caracteres vides  
-    tab = [[" ࠀ " for i in range(3)] for j in range(3)]
+    tab = [[" □ " for i in range(3)] for j in range(3)]
     noWin = True
+    nbTour = 0
     vainqueur = "Le vainqueur est : "
+    nomJoueurA = input("Le nom du joueur 1 : \n")
+    change = False
+    #si il n'y a qu'un joueur
+    if nombreJoueur == 1 :
+        #alors on determine nomJoueurB a "IA"
+        nomJoueurB = "IA"
+    #sinon
+    else :
+        #on assigne a nomJoueurB le string du retour de l'execution de la fonction input
+        nomJoueurB = input("Le nom du joueur 2 : \n")
+
+    symboleJoueurA = input("Symbole du joueur 1 : \n X ou O \n")
+
+    while symboleJoueurA != "X" and symboleJoueurA != "O" :
+        symboleJoueurA = input("Veuillez resaisir le symbole du joueur 1 : \n")
+
+    if symboleJoueurA == "X" :
+        symJouA = " X "
+        symJouB = " O "
+    else :
+        symJouB = " X "
+        symJouA = " O "
+
     for i in tab:
         for j in i:
             print(j, end="")
         print()
+        
     while noWin:
+        nbTour = nbTour + 1 
         print("choixJoueur 1 : \n")
-        ligne = int(input("ligne : \n"))
-        while ligne !=1 and ligne !=2 and ligne !=3 :
-            ligne = int(input("\n Veuillez resaisir la ligne : \n"))
-        #on initialise colonne avec l'appelle de la fonction input
-        colonne = int(input("Colonne : \n"))
-        while colonne !=1 and colonne !=2 and colonne !=3 :
-            ligne = int(input("\n Veuillez resaisir la colonne : \n"))
-        #on affiche tab avec la fonction display
-        lig = ligne-1
-        col = colonne-1
-        tab[lig][col] = " X "
+        while change == False:
+            ligne = input("ligne : \n")
+            while ligne !="1" and ligne !="2" and ligne !="3" :
+                ligne = input("Veuillez resaisir la ligne : \n")
+
+            colonne = input("Colonne : \n")
+            while colonne !="1" and colonne !="2" and colonne !="3" :
+                ligne = input("Veuillez resaisir la colonne : \n")
+            lig = int(ligne)-1
+            col = int(colonne)-1
+            if tab[lig][col] == " □ " :
+                change = True
+            else :
+                print("Case dejà modifiée")
+
+        tab[lig][col] = symJouA
+        change = False
 
         #ligne
         for i in range(0,3):
-            if tab[i][0]==" X " and tab[i][1]==" X " and tab[i][2]==" X " :
-                noWin = False
-                return vainqueur + " X "
+            if tab[i][0]==symJouA and tab[i][1]==symJouA and tab[i][2]==symJouA :
+                for i in tab:
+                    for j in i:
+                        print(j, end="")
+                    print()
+                return vainqueur + nomJoueurA
         
         
         #colonne
         for i in range(0,3):
-            if tab[0][i]==" X " and tab[1][i]==" X " and tab[2][i]==" X " :
-                noWin = False
-                return vainqueur + " X "
+            if tab[0][i]==symJouA and tab[1][i]==symJouA and tab[2][i]==symJouA :
+                for i in tab:
+                    for j in i:
+                        print(j, end="")
+                    print()
+                return vainqueur + nomJoueurA
 
 
         
         #diagonale
-        if tab[0][0]==" X " and tab[1][1]==" X " and tab[2][2]==" X " :
-            noWin = False
-            return vainqueur + " X "
+        if tab[0][0]==symJouA and tab[1][1]==symJouA and tab[2][2]==symJouA or tab[2][0]==symJouA and tab[1][1]==symJouA and tab[0][2]==symJouA :
+            for i in tab:
+                for j in i:
+                    print(j, end="")
+                print()
+            return vainqueur + nomJoueurA
 
-        if tab[2][0]==" X " and tab[1][1]==" X " and tab[0][2]==" X ":
-            noWin = False
-            return vainqueur + " X "
 
         for i in tab:
             for j in i:
                 print(j, end="")
             print()
+        
+        if nbTour == 5 :
+            noWin = False
 
         print("choixJoueur 2 : \n")
-        ligne = int(input("ligne : \n"))
-        #on initialise colonne avec l'appelle de la fonction input
-        colonne = int(input("Colonne : \n"))
-        #on affiche tab avec la fonction display
-        lig = ligne-1
-        col = colonne-1
-        tab[lig][col] = " O "
+        
+        while change == False :
+            ligne = input("ligne : \n")
+            while ligne !="1" and ligne !="2" and ligne !="3" :
+                ligne = input("Veuillez resaisir la ligne : \n")
+            colonne = input("Colonne : \n")
+            while colonne !="1" and colonne !="2" and colonne !="3" :
+                ligne = input("Veuillez resaisir la colonne : \n")
+            lig = int(ligne)-1
+            col = int(colonne)-1
+            if tab[lig][col] == " □ " :
+                    change = True
+            else :
+                print("Case dejà modifiée")
+        tab[lig][col] = symJouB
+        change = False
 
         #ligne
         for i in range(0,3):
-            if tab[i][0]==" O " and tab[i][1]==" O " and tab[i][2]==" O " :
-                noWin = False
-                return vainqueur + " O "
+            if tab[i][0]==symJouB and tab[i][1]==symJouB and tab[i][2]==symJouB :
+                for i in tab:
+                    for j in i:
+                        print(j, end="")
+                    print()
+                return vainqueur + nomJoueurB
         
         
         #colonne
         for i in range(0,3):
-            if tab[0][i]==" O " and tab[1][i]==" O " and tab[2][i]==" O " :
-                noWin = False
-                return vainqueur + " O "        
+            if tab[0][i]==symJouB and tab[1][i]==symJouB and tab[2][i]==symJouB :
+                for i in tab:
+                    for j in i:
+                        print(j, end="")
+                    print()
+                return vainqueur + nomJoueurB    
+    
         #diagonale
-        if  tab[0][0]==" O " and tab[1][1]==" O " and tab[2][2]==" O ":
-            noWin = False
-            return vainqueur + " O "
+        if  tab[0][0]==symJouB and tab[1][1]==symJouB and tab[2][2]==symJouB or tab[2][0]==symJouB and tab[1][1]==symJouB and tab[0][2]==symJouB:
+            for i in tab:
+                for j in i:
+                    print(j, end="")
+                print()
+            return vainqueur + nomJoueurB
 
-        if tab[2][0]==" O " and tab[1][1]==" O " and tab[0][2]==" O ":
-            noWin = False
-            return vainqueur + " O "
             
         for i in tab:
             for j in i:
                 print(j, end="")
             print()
+        
+        if nbTour == 6 :
+            noWin = False
+    return "Egalite"
 
 demande = input("Voulez-vous jouer au morpion ? Yes or No \n")
 while demande == "Yes":
-    print(morpion())
+    print(morpion(2))
     demande = input("Voulez-vous rejouer au morpion ? Yes or No \n")
