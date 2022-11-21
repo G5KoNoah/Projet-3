3940dbf52a4dc7e896b2d3629bfcb3ba90c1aa68
Noah <nhilaire@gaming.tech>
2022-11-21 17:36:35 +0100

    projet

--- morpion.py
+++ morpion.py
@@ -1,9 +1,9 @@
 
 def morpion(nombreJoueur):
+    vainqueur = "Le vainqueur est : "
     tab = [[" □ " for i in range(3)] for j in range(3)]
     noWin = True
     nbTour = 0
-    vainqueur = "Le vainqueur est : "
     nomJoueurA = input("Le nom du joueur 1 : \n")
     change = False
 
@@ -47,18 +47,11 @@ def morpion(nombreJoueur):
         tab[lig][col] = symJouA
         change = False
 
-        """ligne et colonne"""
-        for i in range(0,3):
-            if tab[i][0]==symJouA and tab[i][1]==symJouA and tab[i][2]==symJouA or tab[0][i]==symJouA and tab[1][i]==symJouA and tab[2][i]==symJouA :
-                expli(tab)
-                return vainqueur + nomJoueurA
-           
-        """diagonale"""
-        if tab[0][0]==symJouA and tab[1][1]==symJouA and tab[2][2]==symJouA or tab[2][0]==symJouA and tab[1][1]==symJouA and tab[0][2]==symJouA :
-            expli(tab)
+        c=checkWin(symJouA,tab)
+        expli(tab)
+        if c==symJouA:
             return vainqueur + nomJoueurA
 
-        expli(tab)
         
         if nbTour == 5 :
             return "Egalite"
@@ -134,86 +127,15 @@ def morpion(nombreJoueur):
                         lig=0
                         col=0
                                
+            c=checkIA(symJouA,tab)
+            if c !=None:
+                lig=c[0]
+                col=c[1]
 
-            for i in range(0,3):
-                if tab[i][0]==symJouA and tab[i][1]==symJouA and tab[i][2]==" □ ":
-                    lig=i
-                    col=2
-                if tab[i][0]==symJouA and tab[i][2]==symJouA and tab[i][1]==" □ ":
-                    lig=i
-                    col=1
-                if tab[i][1]==symJouA and tab[i][2]==symJouA and tab[i][0]==" □ ":
-                    lig=i
-                    col=0
-                if tab[0][i]==symJouA and tab[1][i]==symJouA and tab[2][i]==" □ ":
-                    lig=2
-                    col=i
-                if tab[0][i]==symJouA and tab[2][i]==symJouA and tab[1][i]==" □ ":
-                    lig=1
-                    col=i
-                if tab[1][i]==symJouA and tab[2][i]==symJouA and tab[0][i]==" □ ":
-                    lig=0
-                    col=i
-            
-
-            if tab[0][0]==symJouA and tab[1][1]==symJouA and tab[2][2]==" □ ":
-                lig=2
-                col=2 
-            if tab[0][0]==symJouA and tab[2][2]==symJouA and tab[1][1]==" □ ":
-                lig=1
-                col=1 
-            if tab[2][2]==symJouA and tab[1][1]==symJouA and tab[0][0]==" □ ":
-                lig=0
-                col=0
-            if tab[0][2]==symJouA and tab[1][1]==symJouA and tab[2][0]==" □ ":
-                lig=2
-                col=0 
-            if tab[0][2]==symJouA and tab[2][0]==symJouA and tab[1][1]==" □ ":
-                lig=1
-                col=1 
-            if tab[1][1]==symJouA and tab[2][0]==symJouA and tab[0][2]==" □ ":
-                lig=0
-                col=2
-
-            for i in range(0,3):
-                if tab[i][0]==symJouB and tab[i][1]==symJouB and tab[i][2]==" □ ":
-                    lig=i
-                    col=2
-                if tab[i][0]==symJouB and tab[i][2]==symJouB and tab[i][1]==" □ ":
-                    lig=i
-                    col=1
-                if tab[i][1]==symJouB and tab[i][2]==symJouB and tab[i][0]==" □ ":
-                    lig=i
-                    col=0
-                if tab[0][i]==symJouB and tab[1][i]==symJouB and tab[2][i]==" □ ":
-                    lig=2
-                    col=i
-                if tab[0][i]==symJouB and tab[2][i]==symJouB and tab[1][i]==" □ ":
-                    lig=1
-                    col=i
-                if tab[1][i]==symJouB and tab[2][i]==symJouB and tab[0][i]==" □ ":
-                    lig=0
-                    col=i
-            
-
-            if tab[0][0]==symJouB and tab[1][1]==symJouB and tab[2][2]==" □ ":
-                lig=2
-                col=2 
-            if tab[0][0]==symJouB and tab[2][2]==symJouB and tab[1][1]==" □ ":
-                lig=1
-                col=1 
-            if tab[2][2]==symJouB and tab[1][1]==symJouB and tab[0][0]==" □ ":
-                lig=0
-                col=0
-            if tab[0][2]==symJouB and tab[1][1]==symJouB and tab[2][0]==" □ ":
-                lig=2
-                col=0 
-            if tab[0][2]==symJouB and tab[2][0]==symJouB and tab[1][1]==" □ ":
-                lig=1
-                col=1 
-            if tab[1][1]==symJouB and tab[2][0]==symJouB and tab[0][2]==" □ ":
-                lig=0
-                col=2  
+            c=checkIA(symJouB,tab)
+            if c!=None:
+                lig=c[0]
+                col=c[1]
 
         else :
             while change == False:
@@ -232,26 +154,10 @@ def morpion(nombreJoueur):
                     print("Case dejà modifiée")
         tab[lig][col] = symJouB
         change = False
-
-        """Ligne et colonne"""
-        for i in range(0,3):
-            if tab[i][0]==symJouB and tab[i][1]==symJouB and tab[i][2]==symJouB or tab[0][i]==symJouB and tab[1][i]==symJouB and tab[2][i]==symJouB:
-                expli(tab)
-                return vainqueur + nomJoueurB
-          
-    
-        """diagonale"""
-        if  tab[0][0]==symJouB and tab[1][1]==symJouB and tab[2][2]==symJouB or tab[2][0]==symJouB and tab[1][1]==symJouB and tab[0][2]==symJouB:
-            expli(tab)
-            return vainqueur + nomJoueurB
-
         expli(tab)
-    
-def first(tab):
-    for i in range(0,3):
-        for j in range(0,3) :
-            if tab[i][j]== " □ ":
-                return i,j    
+        c=checkWin(symJouB,tab)
+        if c==symJouB:
+            return vainqueur + nomJoueurB
 
 def expli(tab):
     c=[1,2,3]
@@ -263,7 +169,73 @@ def expli(tab):
         print( c[t])
         t=t+1
 
+def checkWin(symJoueur,tab):
+    """ligne et colonne"""
+    for i in range(0,3):
+        if tab[i][0]==symJoueur and tab[i][1]==symJoueur and tab[i][2]==symJoueur or tab[0][i]==symJoueur and tab[1][i]==symJoueur and tab[2][i]==symJoueur :
+            return symJoueur
+    """diagonale"""
+    if tab[0][0]==symJoueur and tab[1][1]==symJoueur and tab[2][2]==symJoueur or tab[2][0]==symJoueur and tab[1][1]==symJoueur and tab[0][2]==symJoueur :
+        return symJoueur
 
+def first(tab):
+    for i in range(0,3):
+        for j in range(0,3) :
+            if tab[i][j]== " □ ":
+                return i,j    
+
+def checkIA(symJoueur,tab):
+    for i in range(0,3):
+        if tab[i][0]==symJoueur and tab[i][1]==symJoueur and tab[i][2]==" □ ":
+            lig=i
+            col=2
+            return lig,col
+        if tab[i][0]==symJoueur and tab[i][2]==symJoueur and tab[i][1]==" □ ":
+            lig=i
+            col=1
+            return lig,col
+        if tab[i][1]==symJoueur and tab[i][2]==symJoueur and tab[i][0]==" □ ":
+            lig=i
+            col=0
+            return lig,col
+        if tab[0][i]==symJoueur and tab[1][i]==symJoueur and tab[2][i]==" □ ":
+            lig=2
+            col=i
+            return lig,col
+        if tab[0][i]==symJoueur and tab[2][i]==symJoueur and tab[1][i]==" □ ":
+            lig=1
+            col=i
+            return lig,col
+        if tab[1][i]==symJoueur and tab[2][i]==symJoueur and tab[0][i]==" □ ":
+            lig=0
+            col=i
+            return lig,col
+
+    if tab[0][0]==symJoueur and tab[1][1]==symJoueur and tab[2][2]==" □ ":
+        lig=2
+        col=2
+        return lig,col 
+    if tab[0][0]==symJoueur and tab[2][2]==symJoueur and tab[1][1]==" □ ":
+        lig=1
+        col=1
+        return lig,col 
+    if tab[2][2]==symJoueur and tab[1][1]==symJoueur and tab[0][0]==" □ ":
+        lig=0
+        col=0
+        return lig,col
+    if tab[0][2]==symJoueur and tab[1][1]==symJoueur and tab[2][0]==" □ ":
+        lig=2
+        col=0
+        return lig,col
+    if tab[0][2]==symJoueur and tab[2][0]==symJoueur and tab[1][1]==" □ ":
+        lig=1
+        col=1
+        return lig,col
+    if tab[1][1]==symJoueur and tab[2][0]==symJoueur and tab[0][2]==" □ ":
+        lig=0
+        col=2
+        return lig,col
+    
 demande = input("Voulez-vous jouer au morpion ? \n")
 if demande=="non":
     print("Dommage :(")
@@ -279,5 +251,3 @@ else:
         while demande != "oui" :
             demande = input("Voulez-vous rejouer au morpion ? \n")
         joueur=int(input("Combien y a t-il de joueur ? \n"))
-
-#regrouper en fonction pour les verifs
